fe7f8e39c353b659f861e1c6103395e0b6e92e7a
s16896 <s16896@pjwstk.edu.pl>
2024-04-28 17:05:33 +0200

    app

---
+++ WebApplication1/Service/IDbService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Task04.Models;
+
+namespace Task04.Services
+{
+    public interface IDbService
+    {
+        Task<int> AddProductToWarehouseAsync(ProductWarehouse productWarehouse);
+    }
+}
